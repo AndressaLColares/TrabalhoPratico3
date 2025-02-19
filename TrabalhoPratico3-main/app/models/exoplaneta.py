26067645e8a48755292679a7b0a398ec81c6a2b4
carlosryan2008 <48930497+carlosryan2008@users.noreply.github.com>
2025-02-19 19:16:46 -0300

    Delete TrabalhoPratico3-main directory

--- TrabalhoPratico3-main/app/models/exoplaneta.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import Document, StringField, ReferenceField, ListField
-
-
-class Exoplaneta(Document):
-    nome = StringField(required=True)
-
-    estrela = ReferenceField('Estrela')  # Relacionamento N:1
-    planetas = ListField(ReferenceField('Planeta'))  # Relacionamento N:N
--- TrabalhoPratico3-main/app/models/fenomeno_celestial.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import Document, StringField, ReferenceField, ListField
-
-class FenomenoCelestial(Document):
-    nome = StringField(required=True)
-    tipo = StringField()
-    descricao = StringField()
-
-    observacoes = ListField(ReferenceField('Observacao'))  # Relacionamento N:N
